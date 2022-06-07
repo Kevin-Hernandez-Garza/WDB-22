1e3c3b6ca1c0e72760d24de7b0b7a2b78bec6b24
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2022-06-07 13:43:35 -0500

    array index knowledge

--- JS/arrays/app.js
+++ JS/arrays/app.js
@@ -1,12 +1,22 @@
-// arrays are indexed and start at 0
 // is type is an object
 
 // array of strings
 let animals = ['lion', 'crocodile', 'whale', 'bobcat', 'giraffe', 'snake'];
+console.log(animals.length); // 6
 
 // empty array 
 let emptyArray = [];
 
 // array of numbers 
 let bingo = [9, 34, 21, 3, 11];
+console.log(bingo.indexOf(11)); // 4
+ 
+// indexing [0, 1, 2, 3] 
+let array = ['texas', 'california', 'oregon', 'new york']; 
+console.log(array.length); //4
+console.log(array[2]); //oregon
+
+// adding value to array
+array[4] = 'north carolina';
+console.log(array);
 
