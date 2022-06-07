8336ca5c08b596a6c27da983f27c2547be4a4bb2
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2022-06-07 02:07:26 -0500

    basic array knowledge

--- JS/arrays/app.js
+++ JS/arrays/app.js
@@ -1,1 +1,12 @@
-console.log('It\'s working');
+// arrays are indexed and start at 0
+// is type is an object
+
+// array of strings
+let animals = ['lion', 'crocodile', 'whale', 'bobcat', 'giraffe', 'snake'];
+
+// empty array 
+let emptyArray = [];
+
+// array of numbers 
+let bingo = [9, 34, 21, 3, 11];
+
