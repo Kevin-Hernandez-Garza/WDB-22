a0b1866c52ada18cfc5dec5062e34014a8a6c6f8
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2022-06-06 23:26:54 -0500

    OR and AND operators knowledge

--- JS/12_Boolean_Logic/Logical_Operators/app.js
+++ JS/12_Boolean_Logic/Logical_Operators/app.js
@@ -18,12 +18,16 @@ if (password.length >= 6 && password.indexOf(' ') === -1) {
 // 65+ free
 
 const ages = 100;
+// greater than or equal to 0 and less than 5 or age is greater or equal to 65 than it's free
 if ((ages >= 0 && ages < 5) || ages >= 65) {
     console.log("FREE");
+// else if ages is greater than or equal to 5 and ages is less than 10 then it's $10
 } else if (ages >= 5 && ages < 10) {
     console.log("$10")
+// else if ages is greater than or equal to 10 and ages is less than 65 then it's $20
 } else if (ages >= 10 && ages < 65) {
     console.log("$20")
+// anything else is an invalid age
 } else {
     console.log("INVALID AGE!")
 };
