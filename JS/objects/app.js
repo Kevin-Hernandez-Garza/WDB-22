458bd340b2b6314d154df3c4caf6ec5719b2d172
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2022-06-10 23:44:38 -0500

    objects structure

--- JS/objects/app.js
+++ JS/objects/app.js
@@ -1,1 +1,8 @@
-console.log('hamm');
+//  object
+const data = {
+    // there is three properties in this object
+    // there are separated by ( key : "value", )
+    name: "Kevin Hernandez",
+    birthday: 02/04/1995, 
+    birth_place: "Austin, Texas"
+};
