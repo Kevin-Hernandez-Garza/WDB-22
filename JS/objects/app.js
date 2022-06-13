9ac153e8a1cd6b4e36b8fd54a0721bc2a75eed9b
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2022-06-12 22:45:03 -0500

    accessing object propertiy values

--- JS/objects/app.js
+++ JS/objects/app.js
@@ -5,8 +5,18 @@ const data = {
     name: "Kevin Hernandez",
     birthday: 02/04/1995, 
     birth_place: "Austin, Texas",
-    isAlive: true,
-    isFunny: false
+    isFat: true,
+    isFunny: false,
+    qualities: ["chubby", "old", "skinny-legs"]
 };
 
 console.log(typeof(data)); //object
+
+
+// accessing data from an array
+console.log(data["isFat"]); //true
+console.log(data["qualities"]); //["chubby", "old", "skinny-legs"]
+
+// better
+console.log(data.isFunny); //false
+console.log(data.name); //"Kevin Hernandez"
